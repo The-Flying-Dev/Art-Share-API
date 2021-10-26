8724113da11179e7e66504ecff74e49176432023
Leandran <leandranm@outlook.com>
2021-10-26 22:11:08 +0200

    add artworks table with model and database validations / constraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211026190749) do
+ActiveRecord::Schema.define(version: 20211026200732) do
+
+  create_table "artworks", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "image_url", null: false
+    t.integer "artist_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
