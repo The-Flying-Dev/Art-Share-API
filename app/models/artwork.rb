beb901aef17b85bd390cb2f96d5c70ec4168ad3b
Leandran <leandranm@outlook.com>
2021-11-08 00:39:12 +0200

    add through associations on User and artwork models

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -4,3 +4,7 @@ class Artwork < ApplicationRecord
     validates :title,  uniqueness: { scope: :artist_id }
     
     belongs_to :artist
+
+    has_many :shared_viewers, through: :artwork_shares, source: :viewer
+      
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     has_many :artworks
+
+    has_many :shared_artworks, through: :artwork_shares, source: :artwork
 end
