adb6154d880982dcd992f1e69518e0bad6d16236
Leandran <leandranm@outlook.com>
2021-11-08 02:18:31 +0200

    create artworks controller

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -3,7 +3,7 @@ class Artwork < ApplicationRecord
     validates :image_url, uniqueness: true
     validates :title,  uniqueness: { scope: :artist_id }
     
-    belongs_to :artist
+    belongs_to :artist, foreign_key: :artist_id, class: 'User'
 
     has_many :shared_viewers, through: :artwork_shares, source: :viewer
       
